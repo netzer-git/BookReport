2dd1cc82693368a316b6211ba6b5a9a6b5d46467
Netzer <Netzerep@gmail.com>
2021-08-30 16:07:28 +0300

    Constants changes

--- xl_preprocess.py
+++ xl_preprocess.py
@@ -2,15 +2,6 @@ import pandas as pd
 from pip._internal.cli.status_codes import SUCCESS
 import Constants
 
-XL_ROOT = 'BookDepository/Full.xlsx'
-XL_AUTHORS_OUTPUT = 'Best_Authors.xlsx'
-XL_PUBLISHERS_OUTPUT = 'Best_Publishers.xlsx'
-AUTHORS_COLUMN_NAME = 'authors'
-PUBLISHERS_COLUMN_NAME = "clean-publisher"
-CATEGORIES_COLUMN_NAME = "categories"
-BESTSELLERS_COLUMN_NAME = 'bestsellers-rank'
-BEST_AUTHOR_RANK = 30_000
-
 
 def split_category_field(category_str):
     category_lst = category_str.split(',')
@@ -23,9 +14,9 @@ def split_category_field(category_str):
 
 def analyze_categories(df):
     categories_dict = {}
-    for i in range(len(df[CATEGORIES_COLUMN_NAME])):
-        category_lst = split_category_field(df[CATEGORIES_COLUMN_NAME][i])
-        rank = df[BESTSELLERS_COLUMN_NAME][i]
+    for i in range(len(df[Constants.CATEGORIES_COLUMN_NAME])):
+        category_lst = split_category_field(df[Constants.CATEGORIES_COLUMN_NAME][i])
+        rank = df[Constants.BESTSELLERS_COLUMN_NAME][i]
         for c in category_lst:
             if c is not None and c in categories_dict:
                 categories_dict[c][0] += rank
@@ -46,7 +37,7 @@ def get_rank_dict(df, column_name):
     rank_dict = {}
     for i in range(len(df[column_name])):
         name = df[column_name][i]
-        rank = df[BESTSELLERS_COLUMN_NAME][i]
+        rank = df[Constants.BESTSELLERS_COLUMN_NAME][i]
 
         if name is not None and name in rank_dict:
             rank_dict[name][0] += rank
@@ -64,7 +55,7 @@ def get_rank_dict(df, column_name):
 def write_binary_big_publishers_column(df, rank_dict):
     big_publishers_lst = [key for key in rank_dict if rank_dict[key][1] > Constants.BIG_PUBLISHERS_BOOK_NUM]
     new_column = []
-    for p in df[PUBLISHERS_COLUMN_NAME]:
+    for p in df[Constants.PUBLISHERS_COLUMN_NAME]:
         new_column.append(int(p in big_publishers_lst))
 
     df['big-publisher-rank'] = new_column
@@ -74,7 +65,7 @@ def write_binary_big_publishers_column(df, rank_dict):
 def write_binary_categories_columns(df, categories_list):
     new_columns = {c: [] for c in categories_list}
     for i in range(len(df['crawl_id'])):
-        entry_categories = split_category_field(df[CATEGORIES_COLUMN_NAME][i])
+        entry_categories = split_category_field(df[Constants.CATEGORIES_COLUMN_NAME][i])
         for c in categories_list:
             new_columns[c].append(int(c in entry_categories))
 
@@ -98,18 +89,18 @@ def preprocess_root_xl(path):
     df = pd.read_excel(path)
     # organize the data
     categories_list = analyze_categories(df)
-    publishers_rank_dict = get_rank_dict(df, PUBLISHERS_COLUMN_NAME)
-    authors_rank_dict = get_rank_dict(df, AUTHORS_COLUMN_NAME)
+    publishers_rank_dict = get_rank_dict(df, Constants.PUBLISHERS_COLUMN_NAME)
+    authors_rank_dict = get_rank_dict(df, Constants.AUTHORS_COLUMN_NAME)
     # write binary columns
     df = write_binary_big_publishers_column(df, publishers_rank_dict)
     df = write_binary_categories_columns(df, categories_list)
     # write numeric columns
-    df = write_numeric_rank_column(df, publishers_rank_dict, PUBLISHERS_COLUMN_NAME)
-    df = write_numeric_rank_column(df, authors_rank_dict, AUTHORS_COLUMN_NAME)
+    df = write_numeric_rank_column(df, publishers_rank_dict, Constants.PUBLISHERS_COLUMN_NAME)
+    df = write_numeric_rank_column(df, authors_rank_dict, Constants.AUTHORS_COLUMN_NAME)
     # writing back to the original file
     df.to_excel(path, index=False)
     return SUCCESS
 
 
 if __name__ == '__main__':
-    preprocess_root_xl(XL_ROOT)
+    preprocess_root_xl(Constants.XL_ROOT)
